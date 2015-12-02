10d58956ea468110a5d2de663fd4e1a6c5261bdb
marwensaid <marwensaidi1@gmail.com>
2015-12-02 14:23:35 +0100

    feat(poi-grails) spike controllerUser perso

--- grails-app/controllers/tdgrails/UserController.groovy
+++ grails-app/controllers/tdgrails/UserController.groovy
@@ -2,7 +2,13 @@ package tdgrails
 
 class UserController {
 
-    def index()
+    def index(Integer max)
     {
+        params.max = Math.min(max ?: 10, 100)
+        respond Utilisateur.list(params), model: [utilisateurInstanceCount: Utilisateur.count()]
+    }
+
+    def show(Utilisateur utilisateurInstance) {
+        respond utilisateurInstance.nom
     }
 }
