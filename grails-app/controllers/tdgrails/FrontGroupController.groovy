77e29789435c955f4096acd07ea596885cc178c8
marwensaid <marwensaidi1@gmail.com>
2015-12-07 10:57:09 +0100

    feat(poi-grails) add implementation of groupController (perso)

--- grails-app/controllers/tdgrails/FrontGroupController.groovy
+++ grails-app/controllers/tdgrails/FrontGroupController.groovy
@@ -1,6 +1,34 @@
 package tdgrails
 
+import grails.transaction.Transactional
+import grails.web.RequestParameter
+
+@Transactional(readOnly = false)
 class FrontGroupController {
 
-    def index() {}
+    String listOfGroups
+
+
+    def index() {
+        [listOfGroups: GroupePois.findAll()]
+    }
+
+    def show() {
+
+    }
+
+    def edit(@RequestParameter('id') String id) {
+        GroupePois groupePois = GroupePois.findById(id)
+        groupePois.nom = params.get("nom")
+        groupePois.pois = params.get("poi")
+        groupePois.imgs = params.get("img")
+        groupePois.save()
+        flash.message = "données modifiées"
+
+
+    }
+
+    def create() {
+        respond new GroupePois(params)
+    }
 }
